48ee1c165b7a3591b4821ec2ec6928983a4320de
6notes <jlu6.proton@proton.me>
2024-03-08 22:07:05 -0800

    Add CRUD API routes

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,16 @@
 import { SSTConfig } from "sst";
+
+import { ApiStack } from "./stacks/ApiStack";
 import { StorageStack } from "./stacks/StorageStack";
 
 export default {
   config(_input) {
     return {
       name: "webregsched",
-      region: "us-east-1",
+      region: "us-west-1",
     };
   },
   stacks(app) {
-    app.stack(StorageStack);
+    app.stack(StorageStack).stack(ApiStack);
   },
 } satisfies SSTConfig;
